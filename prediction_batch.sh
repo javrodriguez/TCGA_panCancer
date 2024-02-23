41843ff7aedbdd71a09c2b41d17073917900a1f6
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2024-02-23 13:49:34 -0500

    Update prediction_batch.sh

--- prediction_batch.sh
+++ prediction_batch.sh
@@ -7,29 +7,11 @@
 
 sample_sheet=/gpfs/data/abl/home/rodrij92/PROJECTS/TCGA_PanCancer/train_model/train/C.Origami_analysis/sample_atac_ctcf.txt
 model=/gpfs/data/abl/home/rodrij92/PROJECTS/TCGA_PanCancer/train_model/train/C.Origami/checkpoints_k562_v2/models/epoch=40-step=12218.ckpt
-target_points=/gpfs/data/abl/home/rodrij92/PROJECTS/TCGA_PanCancer/train_model/train/C.Origami_analysis/chr10_startPos_w2097152.txt
+target_points=/gpfs/data/abl/home/rodrij92/PROJECTS/TCGA_PanCancer/train_model/train/C.Origami_analysis/hg38_startPos_w2097152.txt
 seq=/gpfs/data/abl/home/rodrij92/PROJECTS/TCGA_PanCancer/train_model/train/C.Origami/data/hg38/dna_sequence/
-outdir="prediction_chr10_TCGA"
+outdir="prediction_allChr_TCGA"
 
 cp $sample_sheet sample_sheet.txt
 cp $target_points target_points.txt
-
-#n_samples=`cat sample_sheet.txt| wc -l`
-
-#sbatch --array=1-50 prediction_sample.sh ${model} ${seq} ${outdir}
-#sleep 600
-#sbatch --array=51-100 prediction_sample.sh ${model} ${seq} ${outdir}
-#sleep 600
-#sbatch --array=101-150 prediction_sample.sh ${model} ${seq} ${outdir}
-#sleep 600
-#sbatch --array=151-200 prediction_sample.sh ${model} ${seq} ${outdir}
-#sleep 600
-sbatch --array=201-250 prediction_sample.sh ${model} ${seq} ${outdir}
-sleep 600
-sbatch --array=251-300 prediction_sample.sh ${model} ${seq} ${outdir}
-sleep 600
-sbatch --array=301-350 prediction_sample.sh ${model} ${seq} ${outdir}
-sleep 600
-sbatch --array=351-400 prediction_sample.sh ${model} ${seq} ${outdir}
-sleep 600
-sbatch --array=401-409 prediction_sample.sh ${model} ${seq} ${outdir}
+n_samples=`cat sample_sheet.txt| wc -l`
+sbatch --array=1-$n_samples prediction_sample_point.sh ${model} ${seq} ${outdir}
